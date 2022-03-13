5b98d1b678c1acbeae35dfbe75ff3921459efcb3
Konstantin Krishtapov <constantinopladwar@gmail.com>
2022-03-13 15:43:47 +0100

    3. Realizo enumerador Tramo

--- src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
@@ -2,6 +2,11 @@ package org.iesalandalus.programacion.reservasaulas;
 
 public class MainApp {
 
+	@Override
+	public String toString() {
+		return "MainApp []";
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Programa para la gestión de reservas de espacios del IES Al-Ándalus");
 	}
